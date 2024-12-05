65d4fdd93b0fd6dccd3a59824c2106ce53a620ca
Dan Carroll <dancarroll@gmail.com>
2024-12-05 18:47:59 -0500

    Move graph creation into `day5/shared.dart` in preparation for part 2, and clean up the part 1 solution

--- lib/day5/part_1.dart
+++ lib/day5/part_1.dart
@@ -1,5 +1,4 @@
 import 'package:aoc_2024/lib.dart';
-import 'package:collection/collection.dart';
 
 import 'shared.dart';
 
@@ -10,46 +9,10 @@ import 'shared.dart';
 /// update, and add all of those numbers together.
 Future<int> calculate(Resources resources) async {
   final contents = await loadData(resources);
-  final graph = _loadRulesIntoGraph(contents.rules);
 
   return contents.updates
-      .where((update) => _isUpdateValid(graph: graph, update: update))
+      .where((update) => update.isValid(rules: contents.rules))
       .map((update) => update.middle)
       .reduce((v, e) => v+e);
 }
 
-final class Node {
-  final int page;
-  final List<Node> after = [];
-
-  Node({required this.page});
-}
-
-Map<int, Node> _loadRulesIntoGraph(List<Rule> rules) {
-  final graph = <int, Node>{};
-
-  for (final rule in rules) {
-    final beforeNode =
-        graph.putIfAbsent(rule.before, () => Node(page: rule.before));
-    final afterNode =
-        graph.putIfAbsent(rule.after, () => Node(page: rule.after));
-
-    beforeNode.after.add(afterNode);
-  }
-
-  return graph;
-}
-
-bool _isUpdateValid(
-    {required final Map<int, Node> graph, required final Update update}) {
-  var node = graph[update.pages[0]];
-
-  var i = 0;
-  while (node != null && i < update.pages.length - 1) {
-    i++;
-    final page = update.pages[i];
-    node = node.after.firstWhereOrNull((n) => n.page == page);
-  }
-
-  return (node != null) && i >= update.pages.length - 1;
-}
--- lib/day5/shared.dart
+++ lib/day5/shared.dart
@@ -1,12 +1,49 @@
 import 'package:aoc_2024/lib.dart';
+import 'package:collection/collection.dart';
+
+/// A rules graph is a map of page numbers to page nodes.
+/// The page node contains the page number, and a list of
+/// page nodes that are allowed to appear after that page.
+typedef RulesGraph = Map<int, PageNode>;
+
+/// Represents a node in the rules graph.
+final class PageNode {
+  /// This page's number.
+  final int page;
+
+  /// Thie list of page nodes that are allowed to appear after
+  /// this page.
+  final List<PageNode> after = [];
+
+  PageNode({required this.page});
+}
 
 /// Represents the data for this problem, which is a list of
 /// rules and a list of updates.
 final class Contents {
-  final List<Rule> rules;
+  final RulesGraph rules;
   final List<Update> updates;
 
   Contents({required this.rules, required this.updates});
+
+  /// Constructs a rules graph from a given list of rules, and returns
+  /// a [Contents] instance containing the rules graph and a list of
+  /// updates.
+  factory Contents.fromRulesList(
+      {required List<Rule> rules, required List<Update> updates}) {
+    final graph = <int, PageNode>{};
+
+    for (final rule in rules) {
+      final beforeNode =
+          graph.putIfAbsent(rule.before, () => PageNode(page: rule.before));
+      final afterNode =
+          graph.putIfAbsent(rule.after, () => PageNode(page: rule.after));
+
+      beforeNode.after.add(afterNode);
+    }
+
+    return Contents(rules: graph, updates: updates);
+  }
 }
 
 /// Represents a page rule, which is a declaration that a
@@ -30,9 +67,25 @@ final class Update {
 
   Update({required this.pages});
 
+  /// Returns the middle page value for the update.
   int get middle {
     assert(pages.length.isOdd);
     return pages[(pages.length / 2).floor()];
+  }
+
+  /// Returns true if this update is valid, given the provided
+  /// rules graph.
+  bool isValid({required final RulesGraph rules}) {
+    var node = rules[pages[0]];
+
+    var i = 1;
+    while (node != null && i < pages.length) {
+      final page = pages[i];
+      node = node.after.firstWhereOrNull((n) => n.page == page);
+      if (node != null) i++;
+    }
+
+    return i >= pages.length;
   }
 }
 
@@ -54,6 +107,8 @@ Future<Contents> loadData(Resources resources) async {
       updates.add(
           Update(pages: line.split(',').map((s) => int.parse(s)).toList()));
     } else if (line == '') {
+      // When we encounter the empty line, switch from parsing rules to
+      // parsing updates.
       parsingUpdates = true;
     } else {
       rules.add(
@@ -61,5 +116,5 @@ Future<Contents> loadData(Resources resources) async {
     }
   }
 
-  return Contents(rules: rules, updates: updates);
+  return Contents.fromRulesList(rules: rules, updates: updates);
 }
