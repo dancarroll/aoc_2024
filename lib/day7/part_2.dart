182c3d7a4c522d8cbd07a77172abac287de842b6
Dan Carroll <dancarroll@gmail.com>
2024-12-08 13:08:04 -0500

    Add missing descriptions to the Day 7 solutions, for posterity

--- lib/day7/part_2.dart
+++ lib/day7/part_2.dart
@@ -2,6 +2,8 @@ import 'package:aoc_2024/lib.dart';
 
 import 'shared.dart';
 
+/// Following from part 1, but introduce a new operator:
+/// concatenate (`||`).
 Future<int> calculate(Resources resources) async {
   final equations = await loadData(resources);
 
