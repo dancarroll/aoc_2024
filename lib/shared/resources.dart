bf821197825bbcd7158b42b75856de07b9685bfa
Dan Carroll <dancarroll@gmail.com>
2024-12-08 23:13:36 -0500

    Define the rest of the upcoming days in the day enum

--- lib/shared/resources.dart
+++ lib/shared/resources.dart
@@ -23,7 +23,24 @@ enum Day {
   day5,
   day6,
   day7,
-  day8;
+  day8,
+  day9,
+  day10,
+  day11,
+  day12,
+  day13,
+  day14,
+  day15,
+  day16,
+  day17,
+  day18,
+  day19,
+  day20,
+  day21,
+  day22,
+  day23,
+  day24,
+  day25;
 
   String get number => name.substring(3);
 }
