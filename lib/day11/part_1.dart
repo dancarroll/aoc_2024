840aadc92915b02bbac63ce503d7fac6db99da3a
Dan Carroll <dancarroll@gmail.com>
2024-12-11 21:44:06 -0500

    Update Day 11 Part 1 to use the numDigits and split functions introduced for Part 2
    Add comment block describing the problem

--- lib/day11/part_1.dart
+++ lib/day11/part_1.dart
@@ -5,6 +5,17 @@ import 'shared.dart';
 /// Part 1 is 25 blinks.
 const int numBlinks = 25;
 
+/// --- Day 11: Plutonian Pebbles ---
+///
+/// Given a list of magical stones with values, determine how many stones
+/// are present after blinking 25 times.
+///
+/// Every time you blink, each of the stones changes, according to the
+/// following rules. Each stone is transformed with only the first
+/// applicable rule per blink.
+/// - Stone with value 0 is changed to 1
+/// - Stone wtih an even number of digits is split in two (1234 -> 12, 34)
+/// - Stone value is multiplied by 2024
 Future<int> calculate(File file) async {
   final stones = await loadData(file);
 
@@ -12,13 +23,11 @@ Future<int> calculate(File file) async {
     final numStones = stones.length;
     for (int j = 0; j < numStones; j++) {
       final stone = stones[j];
+      final digits = numDigits(stone);
       if (stone == 0) {
         stones[j] = 1;
-      } else if (stone.toString().length.isEven) {
-        final str = stone.toString();
-        final midpoint = str.length ~/ 2;
-        final stoneLeft = int.parse(str.substring(0, midpoint));
-        final stoneRight = int.parse(str.substring(midpoint));
+      } else if (digits.isEven) {
+        final (stoneLeft, stoneRight) = split(stone, digits: digits);
 
         stones[j] = stoneLeft;
         stones.add(stoneRight);
