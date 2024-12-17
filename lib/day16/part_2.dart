be2e0fa4354c7797c06f63d43e015e814457cd10
Dan Carroll <dancarroll@gmail.com>
2024-12-16 21:26:38 -0500

    Day 16 Part 2 solution
    For some reason, this solution works on my real input, but does not give
    the expected result for the two sample inputs.

---
+++ lib/day16/part_2.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'shared.dart';
+import 'paths.dart';
+
+/// Continuing from Part 1, determine the number of unique points that are
+/// present along at least one lowest cost path (there may be multiple paths
+/// with the same cost).
+///
+/// Return the number of unique points.
+Future<int> calculate(File file) async {
+  final maze = await loadData(file);
+
+  final uniquePoints =
+      findBestPaths(maze).fold(<Point<int>>{}, (v, e) => {...v, ...e.visited});
+  return uniquePoints.length;
+}
