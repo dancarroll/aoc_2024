3d90534b447695d27da0b81d5cfa87af78f0aa68
Dan Carroll <dancarroll@gmail.com>
2024-12-19 23:24:07 -0500

    Day 19 Part 2 solution

--- lib/day19/shared.dart
+++ lib/day19/shared.dart
@@ -1,13 +1,19 @@
 import 'dart:io';
-import 'dart:math' as math;
 
+/// Represents the towels in a hot spring onsen.
 final class Towels {
+  /// List of towels available. Each towel has a set of stripes.
   final List<String> towels;
+
+  /// List of target patterns, which is a sequence of stripes. These patterns
+  /// may be made up of one or more towels, and may not even be feasible to
+  /// construct from the available towels.
   final List<String> patterns;
 
   Towels(this.towels, this.patterns);
 }
 
+/// Loads the towels and patterns from a file.
 Future<Towels> loadData(File file) async {
   final lines = await file.readAsLines();
   final towels = lines[0].split(',').map((s) => s.trim()).toList();
