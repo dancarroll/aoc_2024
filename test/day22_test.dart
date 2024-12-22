cbcdfffe9ec019a43017a405f9ac75f063a1c65e
Dan Carroll <dancarroll@gmail.com>
2024-12-22 15:26:49 -0500

    Day 22 Part 2 solution

--- test/day22_test.dart
+++ test/day22_test.dart
@@ -1,5 +1,6 @@
 import 'package:aoc_2024/lib.dart';
 import 'package:aoc_2024/day22/part_1.dart' as part1;
+import 'package:aoc_2024/day22/part_2.dart' as part2;
 import 'package:test/test.dart';
 
 void main() {
@@ -12,6 +13,10 @@ void main() {
     test('part1', () async {
       expect(await part1.calculate(file), 37327623);
     });
+
+    test('part2', () async {
+      expect(await part2.calculate(file), 24);
+    });
   });
 
   group('real data', tags: 'real-data', () {
@@ -20,6 +25,10 @@ void main() {
 
     test('part1', () async {
       expect(await part1.calculate(file), 15608699004);
+    });
+
+    test('part2', () async {
+      expect(await part2.calculate(file), 1791);
     });
   });
 }
