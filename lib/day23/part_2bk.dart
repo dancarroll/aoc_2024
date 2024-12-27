e9132c3c31892fbd7564c88a5fc39f167344d0c1
Dan Carroll <dancarroll@gmail.com>
2024-12-27 00:59:56 -0500

    Alternative implementation of Day 23 Part 2 using the Bron–Kerbosch algorithm

--- lib/day23/part_2bk.dart
+++ lib/day23/part_2bk.dart
@@ -12,5 +12,37 @@ final setEquals = setEquality.equals;
 Future<String> calculate(File file) async {
   final network = await loadData(file);
 
-  return '';
+  final maxClique = bronKerbosch<String>(network);
+
+  return maxClique.sorted().join(',');
+}
+
+Set<T> bronKerbosch<T>(Map<T, Set<T>> graph) {
+  List<Set<T>> cliques = [];
+  internalBronKerbosch<T>(graph, {}, graph.keys.toSet(), {}, cliques);
+
+  return cliques.reduce(
+      (value, element) => (value.length > element.length) ? value : element);
+}
+
+void internalBronKerbosch<T>(
+    Map<T, Set<T>> graph, Set<T> r, Set<T> p, Set<T> x, List<Set<T>> cliques) {
+  if (p.isEmpty && x.isEmpty) {
+    cliques.add(r);
+    return;
+  }
+
+  final pivot = p.union(x).first;
+
+  for (final vertex in p.difference(graph[pivot]!)) {
+    internalBronKerbosch(
+        graph,
+        r.union({vertex}),
+        p.intersection(graph[vertex]!),
+        x.intersection(graph[vertex]!),
+        cliques);
+
+    p.remove(vertex);
+    x.add(vertex);
+  }
 }
--- test/day23_test.dart
+++ test/day23_test.dart
@@ -1,6 +1,7 @@
 import 'package:aoc_2024/lib.dart';
 import 'package:aoc_2024/day23/part_1.dart' as part1;
 import 'package:aoc_2024/day23/part_2.dart' as part2;
+import 'package:aoc_2024/day23/part_2bk.dart' as part2bk;
 import 'package:test/test.dart';
 
 void main() {
@@ -17,6 +18,10 @@ void main() {
     test('part2', () async {
       expect(await part2.calculate(file), 'co,de,ka,ta');
     });
+
+    test('part2 with Bron–Kerbosch', () async {
+      expect(await part2bk.calculate(file), 'co,de,ka,ta');
+    });
   });
 
   group('real data', tags: 'real-data', () {
@@ -29,6 +34,11 @@ void main() {
 
     test('part2', () async {
       expect(await part2.calculate(file),
+          'ar,cd,hl,iw,jm,ku,qo,rz,vo,xe,xm,xv,ys');
+    });
+
+    test('part2 with Bron–Kerbosch', () async {
+      expect(await part2bk.calculate(file),
           'ar,cd,hl,iw,jm,ku,qo,rz,vo,xe,xm,xv,ys');
     });
   });
